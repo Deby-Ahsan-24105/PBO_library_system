95da4c2936be314834d465ab3efd916f5b77394b
Deby-Ahsan-24105 <debytriaahs@gmail.com>
2026-01-12 19:31:50 +0900

    Add Anggota class with display method

--- anggota.py
+++ anggota.py
@@ -1,1 +1,10 @@
+class Anggota:
+    def __init__(self, nama, nim):
+        self.nama = nama
+        self.nim = nim
 
+    def tampilkan(self):
+        print(f"Nama: {self.nama}, NIM: {self.nim}")
+
+anggota1 = Anggota("Deby Ahsan", "230123456")
+anggota1 .tampilkan()
